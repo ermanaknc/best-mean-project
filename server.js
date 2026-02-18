9d3836de49959af142f58606b77cfa3f60042e71
Erman Akinci <akincierman@gmail.com>
2026-02-18 14:28:23 +0300

    add express and set up server

---
+++ server.js
@@ -0,0 +1,49 @@
+import app from './backend/app.js';
+import debug from 'debug';
+
+const log = debug('node-angular');
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  log('Listening on ' + bind);
+};
+
+const server = app.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
